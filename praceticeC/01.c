3b7a19a66a3cae562ea2fbb67f1a24953a912a17
phu wadon <pmmp_1599@hotmail.com>
2020-08-11 18:21:41 +0700

    Add project files.

---
+++ praceticeC/01.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+    int sum = 0;
+
+    for (int i = 1; i < 1000;i++) {
+        if (i % 3 == 0 && i % 5 == 0) { sum += i; }
+        else if (i % 3 == 0) { sum += i; }
+        else if (i % 5 == 0) { sum += i; }
+        else continue;
+    }
+    printf("%d", sum);
+}
